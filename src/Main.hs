5c95c27c2437e2eb307e7a757b2c934d60fe6e65
Nicholas Dujay <ndujay05@ca.ibm.com>
2015-11-01 16:54:09 -0500

    added repl

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Environment
+import System.Console.Haskeline
 
 import Ast
 import Parser
@@ -10,8 +11,26 @@ import Errors
 import Control.Monad
 import Control.Monad.Except
 
-interpret :: String -> Either LispError LispVal
-interpret input = runExcept $ readExpr input >>= eval
+interpret :: String -> IO String
+interpret input = return $ either show show $ runExcept $ readExpr input >>= eval
+
+evalFile :: String -> IO ()
+evalFile s = readFile s >>= interpret >>= putStrLn
+
+repl :: IO ()
+repl = runInputT defaultSettings loop
+  where
+    loop = do
+      result <- getInputLine "scheme> "
+      case result of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just s -> liftIO (interpret s) >>= outputStrLn >> loop
 
 main :: IO ()
-main = getArgs >>= readFile . head >>= return . interpret >>= print
+main = do
+  args <- getArgs
+  case (length args) of
+    0 -> repl
+    1 -> evalFile $ args !! 0
+    otherwise -> putStrLn "Error! Repl or file mode not selected"
